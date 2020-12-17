7e712b5d19fc346f6e6a3904a6d08e3ccf638472
Katia-2020 <kateflights@gmail.com>
2020-12-17 22:28:01 +0000

    mock array of tracks with all the info

---
+++ client/app/mocks/playlist.js
@@ -0,0 +1,37 @@
+const playlist = [
+  {
+    name: 'Life is a laugh',
+    url:
+      'https://actions.google.com/sounds/v1/human_voices/man_laugh_and_knee_slap.ogg',
+    image: 'https://images.unsplash.com/photo-1533227268428-f9ed0900fb3b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=579&q=80',
+    artist: 'Jimmy Smash',
+  },
+  {
+    name: 'Waiving you goodbye',
+    url:
+      'https://actions.google.com/sounds/v1/cartoon/wooden_train_whistle.ogg',
+    image: 'https://images.unsplash.com/photo-1527684651001-731c474bbb5a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
+    artist: 'Terry Bang',
+  },
+  {
+    name: 'My best friend is a dj',
+    url: 'https://actions.google.com/sounds/v1/animals/dog_snarling.ogg',
+    image: 'https://images.unsplash.com/photo-1587300003388-59208cc962cb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+    artist: 'Keith Collins',
+  },
+  {
+    name: 'Best times are coming',
+    url: 'https://actions.google.com/sounds/v1/cartoon/snare_roll_tumbling.ogg',
+    image: 'https://images.unsplash.com/photo-1567186381030-67ee94d4b6c1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=401&q=80',
+    artist: 'Tom Rabbit',
+  },
+  {
+    name: 'On my way to you',
+    url:
+      'https://actions.google.com/sounds/v1/transportation/low_motorcycle.ogg',
+    image: 'https://images.unsplash.com/photo-1481575184241-4754ea78a1bf?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+    artist: 'Sam Biker',
+  },
+];
+
+export default playlist;
